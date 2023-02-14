700bef648bd8a3ccd1665576c1fc7f0dbfb7dd19
majorsopa <73245063+majorsopa@users.noreply.github.com>
2023-02-13 21:05:34 -0500

    undo

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,14 @@ fn obtain_handle_and_pid() -> HANDLE {
     let process_id_ptr: *mut u32 = &mut process_id;
 
     unsafe {
-        let one_19_2 = CString::new("Minecraft* 1.19.3").unwrap();
+        let one_19_2 = CString::new("Minecraft 1.19.3").unwrap();
         let one_19_2_ptr = one_19_2.as_ptr();
         // this is ugly
         let mut hwnd: HWND = FindWindowA(
             null_mut(),
             one_19_2_ptr,
         );
-        let one_19_2_multiplayer = CString::new("Minecraft* 1.19.3 - Multiplayer (3rd-party Server)").unwrap();
+        let one_19_2_multiplayer = CString::new("Minecraft 1.19.3 - Multiplayer (3rd-party Server)").unwrap();
         let one_19_2_multiplayer_ptr = one_19_2_multiplayer.as_ptr();
         if hwnd == null_mut() {
             hwnd = FindWindowA(
@@ -41,7 +41,7 @@ fn obtain_handle_and_pid() -> HANDLE {
                 one_19_2_multiplayer_ptr,
             );
         }
-        let one_19_2_singleplayer = CString::new("Minecraft* 1.19.3 - Singleplayer").unwrap();
+        let one_19_2_singleplayer = CString::new("Minecraft 1.19.3 - Singleplayer").unwrap();
         let one_19_2_singleplayer_ptr = one_19_2_singleplayer.as_ptr();
         if hwnd == null_mut() {
             hwnd = FindWindowA(
